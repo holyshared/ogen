70615bb654366a8f122eb8367e098a2bcdd6d11e
holyshared <holy.shared.design@gmail.com>
2019-09-13 15:42:31 +0900

    fix version

--- src/cli/default.ml
+++ src/cli/default.ml
@@ -8,12 +8,11 @@
 open Cmdliner
 
 let name = "ogen"
-let version = "0.2.1"
+let version = "0.3.1"
 let doc = "Code generator for OCaml"
 
 let term =
   let sdocs = Manpage.s_common_options in
   let exits = Term.default_exits in
-  (*let man = help_secs in*)
   Term.(ret (const (fun _ -> `Help (`Pager, None)) $ Common_options.gopts_t)),
   Term.info name ~version ~doc ~sdocs ~exits
